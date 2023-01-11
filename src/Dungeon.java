e01ab6edf3cd4ee1dada861abe308291b0ed4c3b
1E10Laptop <sample@bths.edu>
2023-01-11 11:28:40 -0500

    commit

---
+++ src/Dungeon.java
@@ -0,0 +1,6 @@
+public class Dungeon {
+    private final String WATER_EMOJI = "\uD83C\uDF0A";
+    public void say() {
+        System.out.println(WATER_EMOJI);
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Dungeon dun = new Dungeon();
+        dun.say();
+        System.out.println("👍");
+        System.out.println("\uD83D\uDC4D");
+    }
+}
